ee17e6aa8a27689bb36f1dd12d3364865af6220e
vignesh <ssaivignesh7@gmail.com>
2023-01-11 00:13:24 +0530

    Add project files.

---
+++ Counter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Singleton
+{
+    public sealed class Counter
+    {
+        public static int countValue;
+        private Counter()
+        {
+            countValue = 0;
+        }
+
+
+        public static Counter instance = null;
+        public static Counter getInstance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Counter();
+                return instance;
+            }
+        }
+        public int count
+        {
+            get
+            {
+                return countValue;
+            }
+        }
+
+        public void Increment()
+        {
+            countValue++;
+        }
+        public void Decrement()
+        {
+            countValue--;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter obj1 = Counter.getInstance;
+            Counter obj2 = Counter.getInstance;
+                /*
+            Both the obj1 and obj2 are are referring to same instance and this is Singleton design pattern 
+                */
+            obj1.Increment(); //count=1
+            obj2.Decrement(); //count=0
+            obj1.Increment(); //count=1
+
+            int value = obj1.count;
+            Console.WriteLine(value);
+        }
+    }
+}
